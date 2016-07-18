4cd725e462c75cdfad7af43b2edee2ea0b759cc8
DayBreak <forregistersth@163.com>
2016-07-18 23:43:12 +0800

    成员函数和封装

---
+++ Class 类/classTest2.cs
@@ -0,0 +1,62 @@
+//成员函数和封装
+using System;
+
+namespace BoxApplication
+{
+    class Box
+    {
+        private double length;  //长度
+        private double breadth; //宽度
+        private double height;  //高度
+        
+        public void setLength( double len )
+        {
+            length = len;
+        }
+
+        public void setBreadth( double bre)
+        {
+            breadth = bre;
+        }
+
+        public void setHeight( double hei)
+        {
+            height = hei;
+        }
+
+        public double getVolume()
+        {
+            return length * breadth * height;
+        }
+    }
+
+    class Bostester
+    {
+        static void Main(string[] args)
+        {
+            Box Box1 = new Box();   //声明Box1，类型为Box
+            Box Box2 = new Box();   //声明Box2，类型为Box
+            double volume;          //体积
+
+            //Box1详述
+            Box1.setLength(6.0);
+            Box1.setBreadth(7.0);
+            Box1.setHeight(5.0);
+
+            //Box2详述
+            Box2.setLength(12.0);
+            Box2.setBreadth(13.0);
+            Box2.setHeight(10.0);
+       
+            //Box1的体积
+            volume = Box1.getVolume();
+            Console.WriteLine("Box1 的体积： {0}" ,volume);
+
+            //Box2的体积
+            volume = Box2.getVolume();
+            Console.WriteLine("Box2 的体积： {0}", volume);
+
+            Console.ReadKey();
+        }
+    }
+}
